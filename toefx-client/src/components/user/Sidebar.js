a76c19d0c86b6e3cbcec8ba453810b501f61e4cc
Skeli <sesyl.schweitzer@gmail.com>
2021-01-10 18:51:10 -0500

    Clean & Document Nav Bar

--- toefx-client/src/components/user/Sidebar.js
+++ toefx-client/src/components/user/Sidebar.js
@@ -1,29 +1,42 @@
+/*
+    A class for the navigation bar that appears on the side of the dashboard.
+*/
+
 import React, { Component } from 'react'
-import { Container, Button, Row, Navbar, Nav, } from 'react-bootstrap';
-import { connect } from "react-redux";
-import { logOutUser } from "../../Redux/Actions/authAction";
-import store from "../../Redux/store";
 import { isMobile } from 'react-device-detect';
-import dashboardIcon from '../../icons/dashboard2.png';
+import { Container, Button, Row, Navbar, Nav, } from 'react-bootstrap';
+
+import { connect } from "react-redux";
+import store from "../../Redux/store";
+import { logOutUser } from "../../Redux/Actions/authAction";
 import scheduleIcon from '../../icons/appointment.png';
-import logoutIcon from '../../icons/logout.png';
+import dashboardIcon from '../../icons/dashboard2.png';
 import myAccountIcon from '../../icons/myAccount.png';
+import logoutIcon from '../../icons/logout.png';
+
 import '../../componentsStyle/Sidebar.css';
+
 
 class Sidebar extends Component {
     render() {
-        if (isMobile) {
+        var itemClass = "sidebar-items";
+        var activeItemClass = itemClass + " sidebar-active-item"; //Highlight nav link when on page
+        var iconClass = "sidebar-icon";
+        var navLinkClass = "sidebar-item-text";
+
+        if (isMobile) { //Adjust on mobile devices
             return (
                 <Navbar bg="dark" expand="lg" variant="dark">
-
-                    <Navbar.Brand style={{ color: "white" }} onClick={() => {
-                        this.props.history.push('/user');
-                        window.location.reload();
-                    }}>Dashboard</Navbar.Brand>
+                    <Navbar.Brand style={{ color: "white" }} 
+                        onClick={() => {
+                            this.props.history.push('/user');
+                            window.location.reload();
+                        }}
+                    >Dashboard</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Collapse id="basic-navbar-nav"> {/* Hamburger menu */}
                         <Nav className="mr-auto">
-                            <Nav.Link href="#link" onClick={() => { this.props.history.push("/user/schedule") }}>Treatment Schedule</Nav.Link>
+                            <Nav.Link href="#link" onClick={() => this.props.history.push("/user/schedule") }>Treatment Schedule</Nav.Link>
                             <Nav.Link href="#link" onClick={() => this.props.history.push("/user/myAccount")}>My Account</Nav.Link>
                             <Nav.Link href="#link" onClick={() => {
                                 store.dispatch(logOutUser());
@@ -36,61 +49,51 @@ class Sidebar extends Component {
         }
         else {
             return (
-                
                 <div className="sidebar">
+                    {/* Upload Image Button */}
                     <div>
-                        <Button className="new-appointment-button" onClick={() => {
-                            this.props.history.push('/diagnosis');
-                            window.location.reload();
-                        }
-                        }>+ Upload Image</Button>
+                        <Button className="new-appointment-button"
+                            onClick={() => {
+                                this.props.history.push('/diagnosis');
+                                window.location.reload();
+                            }}
+                        >+ Upload Image</Button>
                     </div>
 
+                    {/* Nav Options - Highlight when seleected */}
                     <Container className="main-sidebar-options">
                         {/* Dashboard */}
-                        <Row className={window.location.pathname === "/user" ? "sidebar-items sidebar-active-item" : "sidebar-items"}>
+                        <Row className={window.location.pathname === "/user" ? activeItemClass : itemClass}>
                             <span onClick={() => this.props.history.push("/user")}>
-                                <img src={dashboardIcon} alt="dashboard icon" className="sidebar-icon"></img>
-                                <h6 className="sidebar-item-text">Dashboard</h6>
+                                <img src={dashboardIcon} alt="Dashboard-Icon" className={iconClass}></img>
+                                <h6 className={navLinkClass}>Dashboard</h6>
                             </span>
                         </Row>
 
                         {/* Treatment Schedule */}
-                        <Row className={window.location.pathname === "/user/schedule" ? "sidebar-items sidebar-active-item" : "sidebar-items"}>
+                        <Row className={window.location.pathname === "/user/schedule" ? activeItemClass : itemClass}>
                             <span onClick={() => this.props.history.push("/user/schedule")}>
-                                <img src={scheduleIcon} alt="schedule icon" className="sidebar-icon"></img>
-                                <h6 className="sidebar-item-text">Treatment Schedule</h6>
+                                <img src={scheduleIcon} alt="Schedule-Icon" className={iconClass}></img>
+                                <h6 className={navLinkClass}>Treatment Schedule</h6>
                             </span>
                         </Row>
 
-                        {/* Lab Report */}
-                        {/*<Row className={window.location.pathname === "/user/labReports" ? "sidebar-items sidebar-active-item" : "sidebar-items"}>
-                            <span onClick={() => this.props.history.push("/user/labReports")}>
-                                <img src={reportIcon} alt="report icon" style={{ width: "10%", marginRight: "4%" }} className="sidebar-icon"></img>
-                                <h6 className="sidebar-item-text">Lab Reports</h6>
-                            </span>
-                        </Row>*/}
-
-                    </Container>
-
-                    <Container>
                         {/* My Account */}
-                        <Row className="sidebar-items">
+                        <Row className={window.location.pathname === "/user/myAccount" ? activeItemClass : itemClass}>
                             <span onClick={() => this.props.history.push("/user/myAccount")}>
-                                <img src={myAccountIcon} alt="schedule icon" className="sidebar-icon"></img>
-                                <h6 className="sidebar-item-text">My Account</h6>
+                                <img src={myAccountIcon} alt="Account-Icon" className={iconClass}></img>
+                                <h6 className={navLinkClass}>My Account</h6>
                             </span>
                         </Row>
 
                         {/* Log out */}
-                        <Row className="sidebar-items">
+                        <Row className={itemClass}>
                             <span onClick={() => {
                                 store.dispatch(logOutUser());
                                 window.location.href = "/";
-                            }}
-                            >
-                                <img src={logoutIcon} alt="schedule icon" className="sidebar-icon" style={{ paddingLeft: "3%", marginRight: "5px" }}></img>
-                                <h6 className="sidebar-item-text"> Log Out</h6>
+                            }}>
+                                <img src={logoutIcon} alt="Log-Out-Icon" className={iconClass}></img>
+                                <h6 className={navLinkClass}>Log Out</h6>
                             </span>
                         </Row>
                     </Container>

f7165eb688d4461767b647ceb4667b666be737de
Behzad Khamneli <bkhamneli6@gmail.com>
2021-01-23 16:25:24 -0500

    test cases for login and signup added

--- toefx-server/tests/signup.test.js
+++ toefx-server/tests/signup.test.js
@@ -1,10 +1,13 @@
-const { request } = require("express");
 const mongoose = require("mongoose");
 const UserModel = require('../database/userSchema');
 const userData = {email: 'demoTEST@gmail.com', password: '123test', name: 'tester', age: '12'};
 const config = require('../config');
 const mongodbConfig = require("./mongodb-config");
 const { MongoClient } = require("mongodb");
+
+const request = require("supertest");
+const app = require('../app');
+
 describe('User model test', () => {
     beforeAll(async () => {
         await mongoose.connect(config.database, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
@@ -22,15 +25,50 @@ describe('User model test', () => {
         expect(savedUser.name).toBe(userData.name);
         expect(savedUser.age.toString()).toBe(userData.age);
         expect(savedUser.email).toBe(userData.email);
-    });
-    // Testing input validation
-    // Job is not defined in the user schema
-    it('create and save user with undefined input', async () => {
-        const validUser = new UserModel({email: 'demoTEST@gmail.com', password: '123test', name: 'tester', age: '12', job: "Software engineer"});
-        const savedUser = await validUser.save();
-        expect(savedUser._id).toBeDefined();
-        expect(savedUser.job).toBeUndefined();
+    });    
+});
+
+describe('Signup endpoint', () => {
+    beforeAll(async () => {
+        await mongoose.connect(config.database, { useNewUrlParser: true, useCreateIndex: true }, (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        });
     });
 
+    it('Signup should fail with empty name, email, password and age', async () => {
+        const res = await request(app)
+            .post('/signup')
+            .send({
+                name: "",
+                email: "demoTEST@gmail.com",
+                password: "123",
+                age: "",
+
+            });
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.msg).toBe("Required input is empty");
+    });
+
+    it('Signup should fail if only one input is sent and it is expecting four', async () => {
+        const res = await request(app)
+            .post('/signup').send({})
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.msg).toBe("Required input is undefined");
+    });
+
+    it('Signup should fail if account already exists', async () => {
+        const res = await request(app)
+            .post('/signup').send({
+                name: "Bob Smith",
+                email: "demoTEST@gmail.com",
+                password: "123",
+                age: "22"
+            })
+        expect(res.statusCode).toEqual(400);
+        expect(res.body.msg).toBe("Account already exists");
+    });
     
 });

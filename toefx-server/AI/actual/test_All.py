46c2d46503323a332d631852bfd87dd78e3cd0f7
Skeli <sesyl.schweitzer@gmail.com>
2021-02-07 14:28:34 -0500

    Split Up Python Tests into Indiviual Tests

--- toefx-server/AI/actual/test_All.py
+++ toefx-server/AI/actual/test_All.py
@@ -1,4 +1,6 @@
+import numpy as np
 from pytest import *
+
 from FungalCoverage import *
 from NailExtraction import *
 from NailRecognition import *
@@ -21,40 +23,61 @@ class TestNailRecognition:
         NailRecognition.LoadModel()
         assert model == NailRecognition.model
 
-    def test_GetNailsFromImage_Unit(self):
+    def test_GetNailsFromImage_Unit_1(self):
         assert len(NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "1.jpg")) == 1
+
+    def test_GetNailsFromImage_Unit_2(self):
         assert len(NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "2.jpg")) == 2
+
+    def test_GetNailsFromImage_Unit_3(self):
         assert len(NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "3.jpg")) == 3
+
+    def test_GetNailsFromImage_Unit_4(self):
         assert len(NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "4.jpg")) == 4
+
+    def test_GetNailsFromImage_Unit_5(self):
         assert len(NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "5.jpg")) == 5
 
-    def test_GetNailsFromImage_Stress(self):
+    def test_GetNailsFromImage_Stress_1(self):
         assert len(NailRecognition.GetNailsFromImage("Not Real Path")) == 0
+
+    def test_GetNailsFromImage_Stress_2(self):
         assert len(NailRecognition.GetNailsFromImage(87541)) == 0
+
+    def test_GetNailsFromImage_Stress_3(self):
         assert len(NailRecognition.GetNailsFromImage([TEST_IMG_PATH + RECOGNITION_IMG_PATH + "5.jpg"])) == 0
 
-    def test_DoesImageContainNail_Unit(self):
+    def test_DoesImageContainNail_Unit_1(self):
         nailImages = NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "1.jpg")
         assert NailRecognition.DoesImageContainNail(nailImages) is True
 
+    def test_DoesImageContainNail_Unit_2(self):
         nailImages = NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "2.jpg")
         assert NailRecognition.DoesImageContainNail(nailImages) is True
 
+    def test_DoesImageContainNail_Unit_3(self):
         nailImages = NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "3.jpg")
         assert NailRecognition.DoesImageContainNail(nailImages) is True
 
+    def test_DoesImageContainNail_Unit_4(self):
         nailImages = NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "4.jpg")
         assert NailRecognition.DoesImageContainNail(nailImages) is True
 
+    def test_DoesImageContainNail_Unit_5(self):
         nailImages = NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "5.jpg")
         assert NailRecognition.DoesImageContainNail(nailImages) is True
 
+    def test_DoesImageContainNail_Unit_6(self):
         noNail = NailRecognition.GetNailsFromImage(TEST_IMG_PATH + RECOGNITION_IMG_PATH + "NoNail.jpg")
         assert NailRecognition.DoesImageContainNail(noNail) is False
 
-    def test_DoesImageContainNail_Stress(self):
+    def test_DoesImageContainNail_Stress_1(self):
         assert NailRecognition.DoesImageContainNail("Hi") is False
+
+    def test_DoesImageContainNail_Stress_2(self):
         assert NailRecognition.DoesImageContainNail(55156) is False
+
+    def test_DoesImageContainNail_Stress_3(self):
         assert NailRecognition.DoesImageContainNail((np.array([]), np.array([]))) is False
 
 
@@ -77,19 +100,29 @@ class TestNailExtraction:
         # No dataset is available yet rendering this test currently impossible
         assert False
 
-    def test_GetNailBoundaryPoints_Stress(self):
+    def test_GetNailBoundaryPoints_Stress_1(self):
         assert NailExtraction.GetNailBoundaryPoints("Hi") == []
+
+    def test_GetNailBoundaryPoints_Stress_2(self):
         assert NailExtraction.GetNailBoundaryPoints(55156) == []
+
+    def test_GetNailBoundaryPoints_Stress_3(self):
         assert NailExtraction.GetNailBoundaryPoints((np.array([]), np.array([]))) == []
 
     def test_CreateSegmentedNailImage_Unit(self):
         # No dataset is available yet rendering this test currently impossible
         assert False
 
-    def test_CreateSegmentedNailImage_Stress(self):
+    def test_CreateSegmentedNailImage_Stress_1(self):
         assert NailExtraction.CreateSegmentedNailImage("Hi").size == 0
+
+    def test_CreateSegmentedNailImage_Stress_2(self):
         assert NailExtraction.CreateSegmentedNailImage("Not Real Path").size == 0
+
+    def test_CreateSegmentedNailImage_Stress_3(self):
         assert NailExtraction.CreateSegmentedNailImage(55156).size == 0
+
+    def test_CreateSegmentedNailImage_Stress_4(self):
         assert NailExtraction.CreateSegmentedNailImage((np.array([]), np.array([]))).size == 0
 
 
@@ -108,36 +141,59 @@ class TestFungalCoverage:
         FungalCoverage.LoadModel()
         assert model == FungalCoverage.model
 
-    def test_CalculateCoverage_Unit(self):
+    def test_CalculateCoverage_Unit_1(self):
         assert FungalCoverage.CalculateCoverage(TEST_IMG_PATH + COVERAGE_IMG_PATH + "0.jpg") == 0.0
+
+    def test_CalculateCoverage_Unit_2(self):
         assert abs(FungalCoverage.CalculateCoverage(TEST_IMG_PATH + COVERAGE_IMG_PATH + "1.png") - 20.0) < 0.001
 
-    def test_CalculateCoverage_Stress(self):
+    def test_CalculateCoverage_Stress_1(self):
         # Test random inputs
         assert FungalCoverage.CalculateCoverage("Blah") == 0
+
+    def test_CalculateCoverage_Stress_2(self):
         assert FungalCoverage.CalculateCoverage(75415) == 0
+
+    def test_CalculateCoverage_Stress_3(self):
         assert FungalCoverage.CalculateCoverage([TEST_IMG_PATH + RECOGNITION_IMG_PATH + "5.jpg"]) == 0
 
-    def test_IsNailInfected_Unit(self):
+    def test_IsNailInfected_Unit_1(self):
         assert FungalCoverage.IsNailInfected(0) is False
+
+    def test_IsNailInfected_Unit_2(self):
         assert FungalCoverage.IsNailInfected(0.0) is False
+
+    def test_IsNailInfected_Unit_3(self):
         assert FungalCoverage.IsNailInfected(1) is True
+
+    def test_IsNailInfected_Unit_4(self):
         assert FungalCoverage.IsNailInfected(1.0) is True
+
+    def test_IsNailInfected_Unit_5(self):
         assert FungalCoverage.IsNailInfected(50) is True
+
+    def test_IsNailInfected_Unit_6(self):
         assert FungalCoverage.IsNailInfected(50.0) is True
 
-    def test_IsNailInfected_Acceptance(self):
+    def test_IsNailInfected_Acceptance_1(self):
         coverage = FungalCoverage.CalculateCoverage(TEST_IMG_PATH + COVERAGE_IMG_PATH + "0.jpg")
         assert FungalCoverage.IsNailInfected(coverage) is False
 
+    def test_IsNailInfected_Acceptance_2(self):
         coverage = FungalCoverage.CalculateCoverage(TEST_IMG_PATH + COVERAGE_IMG_PATH + "1.png")
         assert FungalCoverage.IsNailInfected(coverage) is True
 
-    def test_IsNailInfected_Stress(self):
+    def test_IsNailInfected_Stress_1(self):
         assert FungalCoverage.IsNailInfected(float('inf')) is True
+
+    def test_IsNailInfected_Stress_2(self):
         assert FungalCoverage.IsNailInfected(float('-inf')) is False
+
+    def test_IsNailInfected_Stress_3(self):
         assert FungalCoverage.IsNailInfected("Blah") is False
+
+    def test_IsNailInfected_Stress_4(self):
         assert FungalCoverage.IsNailInfected([5]) is False
+
+    def test_IsNailInfected_Stress_5(self):
         assert FungalCoverage.IsNailInfected(True) is False
-
-

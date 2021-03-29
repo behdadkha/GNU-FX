6c3b21e7e36f26f05a28e52d3f09d66ec0393e91
Behdad Khamneli <khamneli.behdad@gmail.com>
2021-03-29 19:21:28 -0400

    new base

--- toefx-server/config.js
+++ toefx-server/config.js
@@ -2,7 +2,7 @@ var os = require("os");
 
 const config = {
     secretKey: "secretKey",
-    database: `mongodb+srv://behdad:Behdad790@user.nurmz.mongodb.net/user?retryWrites=true&w=majority`,
+    database: `mongodb+srv://behdad:ToeFXDev2021@user.nurmz.mongodb.net/user?retryWrites=true&w=majority`,
     hostType: os.type(),
     dev_client : "http://localhost:3000",
     dev_email: "toefxdevteam@gmail.com",

d0c1c74b6a64af55bd341fd41332677992d8d05e
Skeli <sesyl.schweitzer@gmail.com>
2021-03-30 13:18:44 -0400

    React Redux Documentation

--- toefx-client/src/Redux/Reducers/index.js
+++ toefx-client/src/Redux/Reducers/index.js
@@ -2,7 +2,9 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import setFootReducer from './setFootReducer';
 
-
+/*
+    Exports the reducers used by various components.
+*/
 export default combineReducers({
     auth : authReducer,
     foot: setFootReducer

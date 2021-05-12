03704fc104a058c442985246566cbf9deb063733
Behdad Khamneli <khamneli.behdad@gmail.com>
2021-05-12 17:57:05 -0400

    naming fixed

--- gnufx-server/config.js
+++ gnufx-server/config.js
@@ -6,11 +6,11 @@ var os = require("os");
 
 const config = {
     secretKey: "secretKey",
-    database: `mongodb+srv://behdad:toefxDev2021@user.nurmz.mongodb.net/user?retryWrites=true&w=majority`,
+    database: `mongodb+srv://behdad:ToeFXDev2021@user.nurmz.mongodb.net/user?retryWrites=true&w=majority`,
     hostType: os.type(),
     dev_client : "http://localhost:3000",
     dev_email: "toefxdevteam@gmail.com",
-    dev_epass: "toefxdevteam123"
+    dev_epass: "Toefxdevteam123"
 }
 
 module.exports = config;
